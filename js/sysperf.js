1719c6f95cade408733c7fea026f9b52ca66de45
Longmoon <chris@longmoon.com>
2015-06-03 14:45:55 -0700

    Convert drawGraphs to object with public methods.
    The main reason I'm doing this is so that I will be able to refresh
    individual graphs, rather than refreshing all graphs when calling the
    drawGraphs function.

--- js/sysperf.js
+++ js/sysperf.js
@@ -4,62 +4,86 @@ var diskCapacityData = dataGen.genDiskCapacity("week");
 var memoryUsageData = dataGen.genMemoryUsage("week");
 var networkTrafficData = dataGen.genNetworkTraffic("week");
 
-drawGraphs();
-
 $(window).resize(function() {
   accordionPanelWidth = $("#collapseOne").width();
-  drawGraphs();
+  drawGraphs.all();
 });
 
 
-function drawGraphs() {
+var drawGraphs = (function() {
 
-	MG.data_graphic({
-	    description: "CPU Usage",
-	    data: cpuUsageData,
-	    width: accordionPanelWidth,
-	    height: 200,
-	    right: 40,
-	    target: document.getElementById('cpu-usage'),
-	    transition_on_update: false,
-	    x_accessor: 'date',
-	    y_accessor: 'value'
-	});
+	var cpuUsage = function() {
+		MG.data_graphic({
+		    description: "CPU Usage",
+		    data: cpuUsageData,
+		    width: accordionPanelWidth,
+		    height: 200,
+		    right: 40,
+		    target: document.getElementById('cpu-usage'),
+		    transition_on_update: false,
+		    x_accessor: 'date',
+		    y_accessor: 'value'
+		});
+	};
 
-	MG.data_graphic({
-	    description: "Disk Capacity",
-	    data: diskCapacityData,
-	    width: accordionPanelWidth,
-	    height: 200,
-	    right: 40,
-	    target: document.getElementById('disk-capacity'),
-	    transition_on_update: false,
-		x_accessor: 'date',
-	    y_accessor: 'value'
-	});
+	var diskCapacity = function() {
+		MG.data_graphic({
+		    description: "Disk Capacity",
+		    data: diskCapacityData,
+		    width: accordionPanelWidth,
+		    height: 200,
+		    right: 40,
+		    target: document.getElementById('disk-capacity'),
+		    transition_on_update: false,
+			x_accessor: 'date',
+		    y_accessor: 'value'
+		});
+	};
 
-	MG.data_graphic({
-	    description: "Memory Usage",
-	    data: memoryUsageData,
-	    width: accordionPanelWidth,
-	    height: 200,
-	    right: 40,
-	    target: document.getElementById('memory-usage'),
-	    transition_on_update: false,
-	    x_accessor: 'date',
-	    y_accessor: 'value'
-	});
+	var memoryUsage = function() {
+		MG.data_graphic({
+		    description: "Memory Usage",
+		    data: memoryUsageData,
+		    width: accordionPanelWidth,
+		    height: 200,
+		    right: 40,
+		    target: document.getElementById('memory-usage'),
+		    transition_on_update: false,
+		    x_accessor: 'date',
+		    y_accessor: 'value'
+		});
+	};
 
-	MG.data_graphic({
-		description: "Network Traffic",
-	    data: networkTrafficData,
-	    width: accordionPanelWidth,
-		height: 200,
-		right: 40,
-		target: document.getElementById('network-traffic'),
-		transition_on_update: false,
-		x_accessor: 'date',
-		y_accessor: 'value'
-	});
+	var networkTraffic = function() {
+		MG.data_graphic({
+			description: "Network Traffic",
+		    data: networkTrafficData,
+		    width: accordionPanelWidth,
+			height: 200,
+			right: 40,
+			target: document.getElementById('network-traffic'),
+			transition_on_update: false,
+			x_accessor: 'date',
+			y_accessor: 'value'
+		});
+	};
 
-}
+	var all = function() {
+		cpuUsage();
+		diskCapacity();
+		memoryUsage();
+		networkTraffic();
+	}
+
+	return {
+		cpuUsage: cpuUsage,
+		diskCapacity: diskCapacity,
+		memoryUsage: memoryUsage,
+		networkTraffic: networkTraffic,
+		all: all
+	}
+
+})();
+
+
+drawGraphs.all();
